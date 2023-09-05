7e38fcbc162a31d2432c82372c3f1a8d0d51a43b
Jaga <jagblst@gmail.com>
2023-09-05 10:46:37 +0300

    Sample Poll app
    Coding Challenge #1 Completed

--- 10-Functions/Coding Challenge #1/script.js
+++ 10-Functions/Coding Challenge #1/script.js
@@ -35,7 +35,30 @@ GOOD LUCK 😀
 
 const poll = {
   question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  // This generates [0, 0, 0, 0]. More in the next section 😃
+  options: ['1: JavaScript', '2: Python', '3: Rust', '4: C++'],
+  // This generates [0, 0, 0, 0].
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+   const answer = +prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`);
+
+   if (typeof answer === 'number' && answer <= this.answers.length && answer > 0) 
+   this.answers[answer - 1]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
 }
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// BONUS:
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
