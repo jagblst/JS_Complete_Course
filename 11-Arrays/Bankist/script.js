70267ff6c0f3ac114c6e278f1ab58a67bfd14e2e
Jaga <jagblst@gmail.com>
2023-09-12 12:50:27 +0300

    "Bankist" app
    Implement Transfers

--- 11-Arrays/Bankist/script.js
+++ 11-Arrays/Bankist/script.js
@@ -87,9 +87,9 @@ const displayMovements = function (movements) {
   });
 };
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -125,6 +125,14 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
+const updateUI = function(acc){
+  displayMovements(acc.movements);
+
+  calcDisplayBalance(acc);
+
+  calcDisplaySummary(acc);
+}
+
 let currentAccount;
 
 btnLogin.addEventListener('click', function (e) {
@@ -143,12 +151,21 @@ btnLogin.addEventListener('click', function (e) {
 
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
-    displayMovements(currentAccount.movements);
 
-    calcDisplayBalance(currentAccount.movements);
-  
-    calcDisplaySummary(currentAccount);
+    updateUI(currentAccount);
   }
- 
-
 });
+
+btnTransfer.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+})
