6822a51d4f2fec0bed5155c3a368cdff0f36a331
Jaga <jagblst@gmail.com>
2023-09-13 10:57:15 +0300

    "Bankist" app
    Request loan

--- 11-Arrays/Bankist/script.js
+++ 11-Arrays/Bankist/script.js
@@ -170,6 +170,21 @@ btnTransfer.addEventListener('click', (e) => {
   }
 })
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+   
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
