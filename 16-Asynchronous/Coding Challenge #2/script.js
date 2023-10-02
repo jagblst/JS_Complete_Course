e9194d60eed6868461519995d042494e58de732e
Jaga <jagblst@gmail.com>
2023-10-02 13:20:56 +0300

    Asynchronous Coding Challenge #2
    Completed

--- 16-Asynchronous/Coding Challenge #2/script.js
+++ 16-Asynchronous/Coding Challenge #2/script.js
@@ -25,4 +25,46 @@ TEST DATA: Images in the img folder. Test the error handler by passing a wrong i
 GOOD LUCK 😀
 */
 
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
 const imgContainer = document.querySelector('.images');
+
+const createImage = function(imgPath) {
+  return new Promise( function (resolve, reject) {
+    let img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject( new Error('Image not found'));
+    });
+  });
+}
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
