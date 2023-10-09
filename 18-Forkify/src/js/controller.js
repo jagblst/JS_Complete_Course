e8261611ab5a3873dd8a2696d68d090b8d2efe85
Jaga <jagblst@gmail.com>
2023-10-09 15:56:12 +0300

    Forkify app
    Implementing Search Results
    searchView.js added

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -1,18 +1,14 @@
 import * as model from './model.js'; 
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+import { async } from 'regenerator-runtime';
 
 const controlRecipes = async function() {
   try{
     const id = window.location.hash.slice(1);
-    console.log(id);
     if(!id) return;
     recipeView.renderSpinner();
 
@@ -24,7 +20,20 @@ const controlRecipes = async function() {
   }
 };
 
+const controlSearchResults = async function() {
+  try{
+    const query = searchView.getQuery();
+    if(!query) return;
+
+   await model.loadSearchResults(query);
+   console.log(model.state.search.results);
+  }catch(err) {
+    console.log(err);
+  }
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
--- 18-Forkify/src/js/model.js
+++ 18-Forkify/src/js/model.js
@@ -3,12 +3,13 @@ import { API_URL } from "./config";
 import {getJSON } from "./helpers";
 
 export const state = {
-    recipe: {}
+    recipe: {},
+    search: {query: '', results: [],},
 }
 
 export const loadRecipe = async function (id) {
     try {
-        const data = await getJSON(`${API_URL}/${id}`);
+        const data = await getJSON(`${API_URL}${id}`);
 
         const {
             recipe
@@ -25,6 +26,24 @@ export const loadRecipe = async function (id) {
         };
         console.log(state.recipe);
     } catch (err) {
+        console.error(`${err} 💥`);
+        throw err;
+    }
+};
+
+export const loadSearchResults = async function(query) {
+    try{
+        state.search.query = query;
+        const data = await getJSON(`${API_URL}?search=${query}`);
+        state.search.results = data.data.recipes.map(rec => {
+            return{
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                image: rec.image_url,
+            };
+        });
+    } catch(err) {
         console.error(`${err} 💥`);
         throw err;
     }
