66738c36642f27344577cad0d690f85794f89f43
Jaga <jagblst@gmail.com>
2023-10-12 12:03:56 +0300

    Forkify app
    Implementing Bookmarks
    Rendering bookmarks
    Refactoring views files

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -19,6 +20,7 @@ const controlRecipes = async function() {
     recipeView.renderSpinner();
 
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     await model.loadRecipe(id);
    
@@ -59,9 +61,8 @@ const controlAddBookmark =function() {
   } else { 
     model.deleteBookmark(model.state.recipe.id)
   };
-
-  console.log(model.state.recipe);
   recipeView.update(model.state.recipe);
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function() {
